7820d281dc63bb4b172c13b8a0d692b9b6356f59
kenkenman07 <ken0522g@icloud.com>
2025-10-24 09:21:57 +0900

    ログインのバグを修正

--- src/services/signIn.ts
+++ src/services/signIn.ts
@@ -2,7 +2,7 @@ import { supabase } from "./makeSupabase"
 
 const signIn = async (email: string, password: string) => {
     const{ data, error } = await supabase.auth.signInWithPassword({ email, password })
-    if(error) return error
+    if(error) throw error
     return data
 }
 export default signIn
