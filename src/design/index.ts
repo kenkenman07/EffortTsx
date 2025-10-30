54bb354dcd2596cbae791e7eac5e194d68215a27
kenkenman07 <ken0522g@icloud.com>
2025-10-30 11:59:01 +0900

    修正

--- src/design/index.ts
+++ src/design/index.ts
@@ -1,7 +1,5 @@
 export { default as Campfire } from "./components/Campfire";
 export { default as WarmClock } from "./components/WarmClock";
-export type { Friend } from "./components/FriendsPanel";
 export { default as TaskDesign } from "./pages/TaskDesign";
-
 export { default as AppShell } from "./app/AppShell";
 export { default as AppDesignProvider } from "./app/AppDesignProvider";
