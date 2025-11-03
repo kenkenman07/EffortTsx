81227606128833b40ace11fc772e89920138c87e
kenkenman07 <ken0522g@icloud.com>
2025-11-03 14:02:48 +0900

    デザイン修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AppDesignProvider } from './design'
 //import './index.css'
 import Router from './Router'
 
 createRoot(document.getElementById('root')!).render(
-  <AppDesignProvider>
+  
   <StrictMode>
     <Router />
   </StrictMode>,
-  </AppDesignProvider>
+  
 )
